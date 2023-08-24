4f34fa1ece7a245cb2b3cf29388d7fde0763cd8e
stella <sftamesis13@gmail.com>
2023-08-24 13:50:07 -0700

    Complete steps 1-3 out of 4

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Stella Munson';
+// const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
