ef839be2c4382405c075f45d5a19025580fc0c7f
stella <sftamesis13@gmail.com>
2023-08-24 14:00:40 -0700

    Complete

--- index.js
+++ index.js
@@ -3,3 +3,5 @@ const currentUser = 'Stella Munson';
 // const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + '!';
 const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+// const shortGreeting = `Welcome, ${currentUser.charAt(0)}!`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
